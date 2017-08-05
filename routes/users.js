6c75881075daee6ff4a7a8789057c2b8ec9b5edd
Neil Gardose <nkpgardose@gmail.com>
2017-08-06 03:17:17 +0800

    Init commit for dummy api app

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+  res.json([{
+    id: 0,
+    username: 'hello world'
+  }]);
+});
+
+module.exports = router;
